e9e423841578426fdbdaa11c53c723de393efe11
Brian Hestir <brhestir@gmail.com>
2020-12-14 16:04:56 -0500

    Added base layout html and css styling for fill height

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 // main.js
 // The main javascript file for the code-quiz app
 
-var topElement = document.getElementById("#rootH1");
-var newElement = document.createElement("p");
+// var topElement = document.getElementById("#rootH1");
+// var newElement = document.createElement("p");
 
-newElement.textContent = "Hey we have a new <p> element here.";
-document.body.appendChild(newElement);   // Append the <p> element
+// newElement.textContent = "Hey we have a new <p> element here.";
+// document.body.appendChild(newElement);   // Append the <p> element
+
+// function makeBasePage(){
+//   $("#mainDiv").clear();  // reset the div to nothing.
+
+//   var navBarDiv = $("<div>").attr("id", "");   // make a new div
+//   navBarDi
+  
+
+// };
