ecb3871dce4376b76dd299b86efd56038b26df7b
Brian Hestir <brhestir@gmail.com>
2020-12-15 06:19:27 -0500

    Added preliminary answer button event handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,38 +11,38 @@
 
 var questionAnswerArray = [
   [ "Commonly used data types DO NOT include:",
-    [ [ "Strings", false ],
-      [ "Booleans", false ],
-      [ "Alerts", true ],
-      [ "Numbers", false ]
+    [ [ "Strings", "false" ],
+      [ "Booleans", "false" ],
+      [ "Alerts", "true" ],
+      [ "Numbers", "false" ]
     ]
   ],
   [ "The condition in an if / else statement is enclosed within _____.",
-    [ [ "quotes", false ],
-      [ "curly brackets", false ],
-      [ "parentheses", true ],
-      [ "square brackets", false ]
+    [ [ "quotes", "false" ],
+      [ "curly brackets", "false" ],
+      [ "parentheses", "true" ],
+      [ "square brackets", "false" ]
     ]
   ],
   [ "Arrays in JavaScript can be used to store _____.",
-    [ [ "numbers and strings", false ],
-      [ "other arrays", false ],
-      [ "booleans", false ],
-      [ "all of the above", true ]
+    [ [ "numbers and strings", "false" ],
+      [ "other arrays", "false" ],
+      [ "booleans", "false" ],
+      [ "all of the above", "true" ]
     ]
   ],
   [ "String values must be enclosed within _____ when being assigned to variables.",
-    [ [ "commas", false ],
-      [ "curly brackets",false ],
-      [ "quotes", true ],
-      [ "parentheses", false ]
+    [ [ "commas", "false" ],
+      [ "curly brackets", "false" ],
+      [ "quotes", "true" ],
+      [ "parentheses", "false" ]
     ]
   ],
   [ "The Javascript language has what of the following properties?",
-    [ [ "strongly typed", false ],
-      [ "turing complete", false ],
-      [ "piecewise comprehensble", false ],
-      [ "first-class functions", true ]
+    [ [ "strongly typed", "false" ],
+      [ "turing complete", "false" ],
+      [ "piecewise comprehensble", "false" ],
+      [ "first-class functions", "true" ]
     ] 
   ]
 ];
@@ -103,13 +103,37 @@ function displayQuestionCard(){
   headingElement.textContent = questionAnswerArray[questionState][0]; //[0] implies Q part
   mainDiv.append(headingElement);
 
+  // Generate container div for answer buttons buttons
+  var btnContainer = document.createElement("div");
+  btnContainer.className = "d-grid gap-1 btnContainer";
+  mainDiv.append(btnContainer);
+
   // Generate answer button elements from array details
   for(var i=0; i<4; i++){
     var btnElement = document.createElement("button");
-    btnElement.className = "btn btn-primary m-1";
-    btnElement.textContent = questionAnswerArray[questionState][1][i][0]; //[1][0][0] -> answer[0]text
-    mainDiv.append(btnElement);
-  };
+    btnElement.className = "btn btn-primary btn-sm";
+    btnElement.textContent = ""+(i+1)+". " + questionAnswerArray[questionState][1][i][0]; //[1][0][0] -> answer[0]text
+    btnElement.setAttribute("data-index", i);
+    
+    var index = i;
+    btnElement.addEventListener("click", function(event){
+      var btnIndex = event.target.getAttribute("data-index"); // btn index as created earlier
+      if(questionAnswerArray[questionState][1][btnIndex][1] === "true"){
+        console.log("Answer is TRUE!");
+        questionState++;
+      }
+      else if( questionAnswerArray[questionState][1][btnIndex][1] === "false"){
+        console.log("Answer is FALSE!");
+      }
+      else{
+        console.log("Answer is NEITHER TRUE NOR FALSE");
+      }
+      
+    });  // end local event handler
+
+    btnContainer.append(btnElement);
+
+  };  // end for
 
 };
 
