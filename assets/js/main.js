94af6c3ae9140c9fafd8bd001d5b9a9a65446fa5
Brian Hestir <brhestir@gmail.com>
2020-12-15 04:20:12 -0500

    Moved UI population into js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,33 @@
 // main.js
 // The main javascript file for the code-quiz app
 
-// var topElement = document.getElementById("#rootH1");
-// var newElement = document.createElement("p");
+// We want a root element.  This will be the middle row.
 
-// newElement.textContent = "Hey we have a new <p> element here.";
-// document.body.appendChild(newElement);   // Append the <p> element
+// For each "card", we want to 
+// - Consider program state (what card are we on)
+// - What is the form of this card? Intro? Question? HS?
+// - Based on card form, read from data array variable
+// - Generate card using data in the array.
 
-// function makeBasePage(){
-//   $("#mainDiv").clear();  // reset the div to nothing.
+// Obtain, clear, then configure mainDiv root element
+var rootDiv = document.getElementById("rootDiv");
 
-//   var navBarDiv = $("<div>").attr("id", "");   // make a new div
-//   navBarDi
-  
+var mainDiv = document.createElement("div");
+mainDiv.setAttribute("id", "trunkDiv");
+mainDiv.className = "d-flex flex-column align-items-center mt-3";
+rootDiv.append(mainDiv);
 
-// };
+// Obtain and configure content element list
+var headingElement = document.createElement("h3");
+headingElement.textContent = "Coding Quiz Challenge";
+mainDiv.append(headingElement);
+
+var textElement = document.createElement("p");
+textElement.className = "text-center";
+textElement.textContent = "Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+mainDiv.append(textElement);
+
+var btnElement = document.createElement("button");
+btnElement.className = "btn btn-primary";
+btnElement.textContent = "Start Quiz";
+mainDiv.append(btnElement);
